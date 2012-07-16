088e9a2458f06b1726090941c5a0184682a349f9
Joseph Silvestre <joseph.silvestre38@gmail.com>
2012-07-16 14:16:15 +0200

    Fixing logout route

--- web/index.php
+++ web/index.php
@@ -25,7 +25,6 @@ $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../src/views',
-    /*'twig.path' => __DIR__.'/../gw2cread',*/
 ));
 
 // firewall for admin area
@@ -33,7 +32,8 @@ $app['security.firewalls'] = array(
     'admin' => array(
         'pattern' => '^/admin/',
         'form' => array('login_path' => '/login', 'check_path' => '/admin/login_check'),
-        'logout' => array('logout_path' => '/logout'),
+        'logout' => array('logout_path' => '/admin/logout'),
+
         'users' => array(
             'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
         ),
@@ -434,10 +434,6 @@ $app->get('/admin/options', function() use ($app) {
     return $app->redirect('/admin/');
 })->bind('admin_options');
 
-$app->get('/logout', function() use($app) {
-   
-   return $app->redirect('/');
-});
 
 $app->get('/format', function() use($app) {
    
